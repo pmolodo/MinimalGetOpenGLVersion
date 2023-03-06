f69653261f608b7bc352412a714ec854ffce749b
Paul Molodowitch <paul@molodowitch.com>
2023-03-06 11:50:11 -0800

    update tar.gz name to include x86_64

--- build_docker.sh
+++ build_docker.sh
@@ -12,4 +12,4 @@ mkdir -p _build/docker/Release
 id=$(docker create minimal_get_opengl_version) && docker cp $id:/MinimalGetOpenGLVersion/_build/Release/GetOpenGLVersion _build/docker/Release/GetOpenGLVersion && docker rm -v $id
 
 # To create an archive:
-# VER=0.1.0 && pushd _build/docker/Release && tar -czvf MinimalGetOpenGLVersion.v${VER}.manylinux.tar.gz GetOpenGLVersion && popd && mv _build/docker/Release/MinimalGetOpenGLVersion.v${VER}.manylinux.tar.gz .
+# VER=0.1.0 && pushd _build/docker/Release && tar -czvf MinimalGetOpenGLVersion.v${VER}.manylinux-x86_64.tar.gz GetOpenGLVersion && popd && mv _build/docker/Release/MinimalGetOpenGLVersion.v${VER}.manylinux-x86_64.tar.gz .
